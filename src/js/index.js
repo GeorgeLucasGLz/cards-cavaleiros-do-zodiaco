d32f14e900682a7d2c80cde0de5d1fee50daa485
Dev_GLz <134442709+GeorgeLucasGLz@users.noreply.github.com>
2023-05-23 16:43:19 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+
+
+/*
+objetivo 1 - Mudar o tema da página que está com uma imagem para o modo escuro solido, com um fundo 
+escuro preto
+
+passo 1 - identificar no JS o botão html que altera o tema;
+passo 2 - Dar um jeito de pegar o body no html para que possa ser alterado o tema;
+passo 3 - Dar um jeito de identificar o clique do usúario no botão;
+passo 4 - Alterar o tema
+passo 5 - Alterar o icone de Galáxia pra Lua;
+
+
+*/
+
+//passo 1 - identificar no JS o botão html que altera o tema;
+
+/*const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+//console.log(botaoAlterarTema);
+
+//passo 2 - Dar um jeito de pegar o body no html para que possa ser alterado o tema;
+
+const body = document.querySelector("body");
+//console.log(body);
+
+//passo 3 - Dar um jeito de identificar o clique do usúario no botão;
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    console.log("OLHA A PEDRA, CLICKOU??? (SIM)?? OU (NÃO)???");
+
+    //passo 4 - Alterar o tema
+
+    body.classList.add("modo-escuro");
+});
+
+*/
